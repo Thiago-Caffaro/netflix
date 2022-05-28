d143ffa63c04b605f4d2b8ab43007959bd5f4f31
Cogumelinho <96150887+Thiago-Caffaro@users.noreply.github.com>
2022-05-28 17:30:07 -0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const listaFilmes = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11"
+];
+
+function informacao(filme) {
+  window.open(listaFilmes[filme]);
+}
